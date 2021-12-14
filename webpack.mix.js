8621e036e3d501d2aa2c06ad9004379ae019962c
udavspokoinyi <60442562+udavspokoinyi@users.noreply.github.com>
2021-12-14 14:55:53 +0500

    version 1.0.0
    Designed and tested

---
+++ webpack.mix.js
@@ -0,0 +1,15 @@
+const mix = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel applications. By default, we are compiling the CSS
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix.js('resources/js/slept.js', 'public/js/slept.js')
+    .css('resources/css/style.css', 'public/css/style.css');
